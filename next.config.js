fde1885050049c6a1895d682646f306e329e43b4
queryops <gallilloss@gmail.com>
2026-02-19 23:06:09 -0500

    update

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: 'ORB-PRUEBA',
+  assetPrefix: 'ORB-PRUEBA',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
